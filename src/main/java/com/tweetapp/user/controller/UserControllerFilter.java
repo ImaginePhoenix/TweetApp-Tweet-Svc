55b8da50a2e091ed7fa2a3836ae107e7534b4106
ImaginePhoenix <karmakar.rahit07@gmail.com>
2022-12-30 20:52:06 +0530

    Added cors

---
+++ src/main/java/com/tweetapp/user/controller/UserControllerFilter.java
@@ -0,0 +1,24 @@
+package com.tweetapp.user.controller;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class UserControllerFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		response.addHeader("Access-Control-Allow-Origin", "*");
+        filterChain.doFilter(request, response);
+
+	}
+
+}
